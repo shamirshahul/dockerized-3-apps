5a1fd293c6f6135b9711d192bd2ac5d2bfe297c8
shamirshahul <shamir@nuevezo.com>
2021-01-17 14:11:10 +0400

    Initial commit after dockerization

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { Guest } from "./entities/Guest";
+import { createConnection, Connection } from "typeorm";
+import { User } from "./entities/User";
+import { Muser } from "./entities/Muser";
+import path from "path";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+createConnection({
+  type: "postgres",
+  host: "postgres",
+  port: 5432,
+  username: "shamir",
+  password: "shamir@123",
+  //database: "qr-app-db",
+  entities: [Guest, User, Muser],
+  synchronize: true,
+}).then((connection) => {
+  require("./services/qr-services")(app, connection);
+  require("./services/user.services")(app, connection);
+  require("./services/static.services")(app);
+});
+
+app.listen(3000, "0.0.0.0");
---
+++ src/services/static.services.ts
@@ -0,0 +1,7 @@
+import { Application } from "express";
+import express from "express";
+module.exports = (app: Application) => {
+  app.use("/visitorkiosk", express.static("visitorkiosk"));
+  app.use("/covidq", express.static("covidq"));
+  app.use("/hostapp", express.static("hostapp"));
+};
